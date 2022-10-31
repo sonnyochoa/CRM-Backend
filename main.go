c86533bf6bd68d974e6ff1996b6a140dbd14e613
Sonny Ochoa <0xsonny@gmail.com>
2022-10-31 12:32:05 -0700

    Seed of customers added. GET request to /customers returns customer list in json.

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/sonnyochoa/CRM-Backend/models"
 )
+
+var customers = []models.Customer{
+	{
+		ID:        1,
+		Name:      "sonny",
+		Role:      "admin",
+		Email:     "admin@sonny.com",
+		Phone:     "1234567890",
+		Contacted: false,
+	},
+	{
+		ID:        2,
+		Name:      "karokomako",
+		Role:      "writer",
+		Email:     "writer@sonny.com",
+		Phone:     "5012346789",
+		Contacted: false,
+	},
+	{
+		ID:        3,
+		Name:      "churikua",
+		Role:      "builder",
+		Email:     "builder@sonny.com",
+		Phone:     "3012456789",
+		Contacted: true,
+	},
+}
 
 func GetHome(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
@@ -13,7 +42,9 @@ func GetHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetCustomers(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world\n")
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(customers)
 }
 
 func main() {
