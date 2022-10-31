9379016e883d91663692061de5e4e586327911ee
Sonny Ochoa <0xsonny@gmail.com>
2022-10-31 12:33:25 -0700

    Added json struct tags.

--- models/customer.go
+++ models/customer.go
@@ -1,10 +1,10 @@
 package models
 
 type Customer struct {
-	ID        int
-	Name      string
-	Role      string
-	Email     string
-	Phone     string
-	Contacted bool
+	ID        int    `json:"id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Role      string `json:"role,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Phone     string `json:"phone,omitempty"`
+	Contacted bool   `json:"contacted,omitempty"`
 }
