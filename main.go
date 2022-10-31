e5d575136191cf8ba7a4e9c0663e15f9640317e6
Sonny Ochoa <0xsonny@gmail.com>
2022-10-31 13:32:53 -0700

    Changed the ds for customers to a map. Added a router and function to get a customer using thier id.

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 	"github.com/sonnyochoa/CRM-Backend/models"
 )
 
-var customers = []models.Customer{
-	{
+var customers = map[string]models.Customer{
+	"1": {
 		ID:        1,
 		Name:      "sonny",
 		Role:      "admin",
@@ -18,7 +18,7 @@ var customers = []models.Customer{
 		Phone:     "1234567890",
 		Contacted: false,
 	},
-	{
+	"2": {
 		ID:        2,
 		Name:      "karokomako",
 		Role:      "writer",
@@ -26,7 +26,7 @@ var customers = []models.Customer{
 		Phone:     "5012346789",
 		Contacted: false,
 	},
-	{
+	"3": {
 		ID:        3,
 		Name:      "churikua",
 		Role:      "builder",
@@ -37,12 +37,26 @@ var customers = []models.Customer{
 }
 
 func GetHome(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html")
+	w.Header().Set("Content-Type", "text/html")
 	http.ServeFile(w, r, "./templates/home.gohtml")
 }
 
+func GetCustomerWithID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id := mux.Vars(r)["id"]
+
+	if _, ok := customers[id]; ok {
+		w.WriteHeader(http.StatusFound)
+		json.NewEncoder(w).Encode(customers[id])
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("{User does not exist}")
+	}
+}
+
 func GetCustomers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(customers)
 }
@@ -51,6 +65,7 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", GetHome).Methods("GET")
+	r.HandleFunc("/customers/{id}", GetCustomerWithID).Methods("GET")
 	r.HandleFunc("/customers", GetCustomers).Methods("GET")
 	fmt.Println("Starting server on localhost:3000...")
 	http.ListenAndServe(":3000", r)
