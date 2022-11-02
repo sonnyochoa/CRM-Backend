fa21c96612ba8479163974d2ad471a84a9bd5505
Sonny Ochoa <0xsonny@gmail.com>
2022-11-01 22:48:47 -0700

    Initalize the in-memory db. Refactored out the handler methods.

--- main.go
+++ main.go
@@ -4,146 +4,47 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"strconv"
+	"strings"
 
-	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"github.com/sonnyochoa/CRM-Backend/controllers"
 	"github.com/sonnyochoa/CRM-Backend/models"
 )
 
-var customers = map[string]models.Customer{
-	"1": {
-		ID:        1,
-		Name:      "sonny",
-		Role:      "admin",
-		Email:     "admin@sonny.com",
-		Phone:     "1234567890",
-		Contacted: false,
-	},
-	"2": {
-		ID:        2,
-		Name:      "panda",
-		Role:      "writer",
-		Email:     "writer@sonny.com",
-		Phone:     "5012346789",
-		Contacted: false,
-	},
-	"3": {
-		ID:        3,
-		Name:      "bear",
-		Role:      "builder",
-		Email:     "builder@sonny.com",
-		Phone:     "3012456789",
-		Contacted: true,
-	},
-}
+var customers map[string]models.Customer
 
-// getHome serves the home page from a static file.
-func getHome(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	http.ServeFile(w, r, "./templates/home.gohtml")
-}
+func initModel() error {
+	customerDB, err := ioutil.ReadFile("./models/customers.json")
 
-// getCustomers returns all customers in JSON format.
-func getCustomers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(customers)
-}
-
-// getCustomer returns a customer object in JSON format from the passed in user ID.
-// If the user ID is not found, it returns nil and StatusNotFound.
-func getCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	id := mux.Vars(r)["id"]
-
-	if _, ok := customers[id]; ok {
-		w.WriteHeader(http.StatusFound)
-		json.NewEncoder(w).Encode(customers[id])
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(nil)
+	if err != nil {
+		log.Fatal(err)
+		return err
 	}
-}
-
-// addCustomer creates a new customer with a unique ID based on the user data submitted.
-// If it's unable to umarshal the post request, it logs an error and returns
-// StatusBadRequest.
-func addCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	uuid := uuid.New()
-	id := uuid.ID()
-
-	var customer models.Customer
-
-	strId := strconv.FormatUint(uint64(id), 10)
-	customer.ID = id
-
-	body, _ := ioutil.ReadAll(r.Body)
-	if err := json.Unmarshal(body, &customer); err != nil {
-		http.Error(w, "Cannot unmarshal post request", http.StatusBadRequest)
-	}
-	customers[strId] = customer
-
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(customers[strId])
-}
-
-// updateCustomer updates the customer based on a user ID. It receives a JSON object
-// with the fields that need updating. It returns nil and StatusNotFound if the user ID
-// does not exist.
-func updateCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	var customer models.Customer
-	userId := mux.Vars(r)["id"]
-
-	if _, ok := customers[userId]; ok {
-		body, _ := ioutil.ReadAll(r.Body)
-		if err := json.Unmarshal(body, &customer); err != nil {
-			http.Error(w, "Cannot unmarshal put request", http.StatusNotFound)
-		} else {
-			customerID, _ := strconv.ParseUint(userId, 10, 32)
-			customer.ID = uint32(customerID)
-			customers[userId] = customer
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(customer)
+	dec := json.NewDecoder(strings.NewReader(string(customerDB)))
+	for dec.More() {
+		err = dec.Decode(&customers)
+		if err != nil {
+			panic(err)
 		}
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(nil)
 	}
-}
-
-// deleteCustomer deletes a customer based on a user ID.  It returns nil and
-// StatusNotFound if the user ID does not exist.
-func deleteCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	userId := mux.Vars(r)["id"]
-
-	if _, ok := customers[userId]; ok {
-		delete(customers, userId)
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(customers)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(nil)
-	}
+	return nil
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", getHome).Methods("GET")
-	r.HandleFunc("/customers", getCustomers).Methods("GET")
-	r.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
-	r.HandleFunc("/customers", addCustomer).Methods("POST")
-	r.HandleFunc("/customers/{id}", updateCustomer).Methods("PUT")
-	r.HandleFunc("/customers/{id}", deleteCustomer).Methods("DELETE")
+	err := initModel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	r.HandleFunc("/", controllers.GetHome).Methods("GET")
+	r.HandleFunc("/customers", controllers.GetCustomers(customers)).Methods("GET")
+	r.HandleFunc("/customers/{id}", controllers.GetCustomer(customers)).Methods("GET")
+	r.HandleFunc("/customers", controllers.AddCustomer(customers)).Methods("POST")
+	r.HandleFunc("/customers/{id}", controllers.UpdateCustomer(customers)).Methods("PUT")
+	r.HandleFunc("/customers/{id}", controllers.DeleteCustomer(customers)).Methods("DELETE")
 	fmt.Println("Starting server on localhost:3000...")
 	http.ListenAndServe(":3000", r)
 }
